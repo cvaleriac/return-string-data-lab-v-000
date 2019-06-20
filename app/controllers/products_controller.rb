5ff5d9f92d1e5266f60cf1bd6ad8f7b61bf34867
valeria campilongo <cvaleriac@hotmail.it>
2019-06-20 14:56:34 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,22 @@ end
 def show
 end
 
+def new
+  @product = Product.new
+end
+
+def create
+  @product = Product.create(product_params)
+  @product.save
+  redirect_to product_path(@product)
+end
+
   def body
     product = Product.find(params[:id])
     render plain: product.description
   end
 
+  def product_params
+      params.require(:product).permit(:name, :description)
+    end
 end
