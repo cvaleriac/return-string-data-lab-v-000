024afeb6a84af38dcbc067a7345606543abc627d
valeria campilongo <cvaleriac@hotmail.it>
2019-06-20 15:06:41 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :invoices
   resources :products
-  get '/products/:id/body', to: 'products#body'
+
+  get '/products/:id/inventory', to: "products#inventory"
+  get 'products/:id/description', to: "products#description"
 end
