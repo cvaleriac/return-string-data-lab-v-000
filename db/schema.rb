d5f6c405645b8a9c8a643d1cc8747fd89b71392e
valeria campilongo <cvaleriac@hotmail.it>
2019-06-20 16:01:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190620142345) do
+ActiveRecord::Schema.define(version: 20190620155949) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20190620142345) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
-    t.string   "inventory"
+    t.integer  "inventory"
   end
 
 end
